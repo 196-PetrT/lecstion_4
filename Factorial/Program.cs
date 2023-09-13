e2efaf2d59ff851c4bba52c3f38a0a595a2a701a
PetrT <140140070+196-PetrT@users.noreply.github.com>
2023-09-13 22:25:53 +0300

    recursii and matrix

---
+++ Factorial/Program.cs
@@ -0,0 +1,19 @@
+﻿// выводи значение факториала
+
+int Factorial(int n) // double и т.д.
+{
+    // 1! = 1
+    // 0! = 1
+    if (n == 1)
+        return 1;
+    else
+        return n * Factorial(n - 1);
+}
+Console.WriteLine(Factorial(3));
+
+// 1 * 2 * 3 = 6
+for (int i = 1; i < 40; i++) // вызвалидля небольших чисел
+{
+    // Console.WriteLine(Factorial(i));
+    Console.WriteLine($"{i}! = {Factorial(i)}"); // посмотрим до какого числа считает, затем нужно менять тип данных
+}
---
+++ doubleArray/Program.cs
@@ -0,0 +1,51 @@
+﻿// 1
+
+string[,] table = new string[2, 5];
+
+// String.Empty
+// table[0,0] table[0,1] table[0,2]
+// table[1,0] table[1,1] table[1,2]
+// table[2,0] table[2,1] table[2,2]
+// table[1, 2] = "word";
+
+// for (int rows = 0; rows < 2; rows++)
+// {
+//     for (int columns = 0; columns < 5; columns++)
+//     {
+//        Console.WriteLine($" - {table[rows, columns]}");
+//     }
+// }
+
+
+
+void PrintArray(int[,] array) // выводит матрицу
+{
+    // for (int i = 0; i < 3; i++)
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        // for (int j = 0; j < 4; j++)
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+// добавим метод который будет заполнять матрицу случайными числами
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10); // генератор псевдослучайдых чисел от [1 до 10)
+        }
+    }
+}
+
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
